fff818107aa96065d05472d82c260275be2c3658
dev.ponomarevss <dev.ponomarevss@gmail.com>
2025-03-21 19:17:51 +0500

    sign up screen reworked

--- app/src/main/java/ru/sspo/focussignup/repository/RoomUserRepository.kt
+++ app/src/main/java/ru/sspo/focussignup/repository/RoomUserRepository.kt
@@ -3,7 +3,7 @@ package ru.sspo.focussignup.repository
 import ru.sspo.focussignup.repository.room.User
 import ru.sspo.focussignup.repository.room.UserDao
 
-class RoomUserRepository(private val userDao: UserDao) : UserRepository{
+class RoomUserRepository(private val userDao: UserDao) : UserRepository {
     override suspend fun getUserByEmail(email: String): User? {
         return userDao.getUserByEmail(email)
     }
